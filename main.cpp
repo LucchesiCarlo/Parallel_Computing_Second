20c5be1f71f75d4e0bb4f3e0cf21048114e66f53
Carlo Lucchesi <carlo.lucchesi@edu.unifi.it>
2026-01-16 12:44:49 +0100

    Inclusion of OpenCV in CMakeLists for image reading. Inside the main.cpp file there is a little demo on how to read, write and access raw data of an image.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
+#include "opencv2/core.hpp"
+#include <opencv2/core/mat.hpp>
+#include <opencv2/imgcodecs.hpp>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    cv::Mat inputImg = cv::imread("image.png", cv::IMREAD_UNCHANGED);
+
+    if (inputImg.empty()) {
+        std::cerr << "Error: Image not found!" << std::endl;
+        return -1;
+    }
+
+    cv::Mat outputImg = cv::Mat::zeros(inputImg.size(), inputImg.type());
+
+    //Access raw bytes of the image
+    auto inputPtr = inputImg.ptr();
+
+
+    outputImg.data = inputPtr;
+    cv::imwrite("output.png", outputImg);
 }
