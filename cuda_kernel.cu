a5b198a0991737e726a6a845bd06bc49eb4c8968
Giacomo-Orsucci <orsucci.giacomo@gmail.com>
2026-01-20 16:47:01 +0100

    First functioning Cuda Version.

--- cuda_kernel.cu
+++ cuda_kernel.cu
@@ -2,7 +2,7 @@
 // Created by giacomo on 1/19/26.
 //
 
-#include "kernel_functions.h"
+#include "src/kernel_functions.h"
 
 __device__ inline unsigned char cudaClamping(float x) {
     if (x < 0.0f) x = 0.0f;
@@ -15,22 +15,24 @@ __device__ inline unsigned char cudaClamping(float x) {
 __global__  void applyCudaKernel(unsigned char* in, unsigned char* out, float* kernel, int K, int W, int H, int C) {
     int center = K / 2;
 
-    for (int y = 0; y < H; y++){
-        for (int x = 0; x < W; x++) {
-            for (int c = 0; c < C; c++) {
-                float result = 0;
-                for (int i = 0; i < K; i++) {
-                    for (int j = 0; j < K; j++) {
-                        const int xIdx = j - center + x;
-                        const int yIdx = i - center + y;
-                        if (!(xIdx < 0 || xIdx >= W || yIdx < 0 || yIdx >= H)) {
-                            const int inputIdx = yIdx * W * C + xIdx * C + c;
-                            result += kernel[i * K + j] * in[inputIdx];
-                        }
-                    }
+    int x = blockIdx.x * blockDim.x + threadIdx.x;
+    int y = blockIdx.y * blockDim.y + threadIdx.y;
+
+    if (x >= W || y >= H) {return;}
+
+    for (int c = 0; c < C; c++) {
+        float result = 0;
+        for (int i = 0; i < K; i++) {
+            for (int j = 0; j < K; j++) {
+                const int xIdx = j - center + x;
+                const int yIdx = i - center + y;
+                if (!(xIdx < 0 || xIdx >= W || yIdx < 0 || yIdx >= H)) {
+                    const int inputIdx = yIdx * W * C + xIdx * C + c;
+                    result += kernel[i * K + j] * in[inputIdx];
                 }
-                out[y * W * C + x * C + c] = clamping(result);
             }
         }
+        out[y * W * C + x * C + c] = cudaClamping(result);
     }
+
 }
--- download_dataset.sh
+++ download_dataset.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 if [ ! -f ./dataset/intel-image-classification.zip ]; then
+    mkdir dataset
     curl -L -o ./dataset/intel-image-classification.zip\
      https://www.kaggle.com/api/v1/datasets/download/puneet6060/intel-image-classification
 fi
